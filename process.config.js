61365cedaaea23d3603e0c5e2e0c777b6b89a1a7
ganatan <ganatanbriel@gmail.com>
2018-08-04 15:26:28 +0200

    update process.config

--- process.config.js
+++ process.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps : [
       {
-        name      : 'seed',
+        name      : 'starter',
         script    : 'dist/server',
         env: {
           COMMON_VARIABLE: 'true'
