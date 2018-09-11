318c846c3b77900d675df4a37cbde4de4f0b9415
ganatan <ganatanbriel@gmail.com>
2018-09-11 14:30:21 +0200

    update SEO

--- src/app/modules/about/about.component.ts
+++ src/app/modules/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private meta: Meta,
+    private titleService: Title,
+  ) { }
 
   ngOnInit() {
+    this.titleService.setTitle('angular-starter : Titre de la page About');
+    this.meta.addTag({
+      name: 'author',
+      content: 'chendra'
+    });
+    this.meta.updateTag(
+      {
+        name: 'description',
+        content: 'angular-starter : Description de la page About'
+      });
   }
 
 }
--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private meta: Meta,
+    private titleService: Title,
+  ) { }
 
   ngOnInit() {
+    this.titleService.setTitle('angular-starter : Titre de la page Home');
+    this.meta.addTag({
+      name: 'author',
+      content: 'chendra'
+    });
+    this.meta.updateTag(
+      {
+        name: 'description',
+        content: 'angular-starter : Description de la page Home'
+      });
   }
 
 }
