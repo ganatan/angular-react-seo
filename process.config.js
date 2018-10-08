5d01d85642f20fe79c2867204725a4078e888837
ganatan <ganatanbriel@gmail.com>
2018-10-08 14:17:27 +0200

    update process config

--- process.config.js
+++ process.config.js
@@ -1,18 +1,17 @@
 module.exports = {
-    apps : [
-      {
-        name      : 'angular-starter',
-        script    : 'app',
-        env: {
-          COMMON_VARIABLE: 'true'
-        },
-        env_dev : {
-          NODE_ENV: 'dev'
-        },
-        env_prod : {
-          NODE_ENV: 'prod'
-        }
+  apps : [
+    {
+      name      : 'angular-starter',
+      script    : 'dist/server',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_dev : {
+        NODE_ENV: 'dev'
+      },
+      env_prod : {
+        NODE_ENV: 'prod'
       }
-    ],
-  };
-  
+    }
+  ],
+};
