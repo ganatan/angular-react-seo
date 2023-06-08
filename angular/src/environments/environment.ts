3361ed05502e6f7da3d2bc683c6f4fd18513ac2b
ganatan <dannyganatan@gmail.com>
2023-06-08 11:37:01 +0200

    Update Angular 16.0.4 & Angular CLI 16.0.5

--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   application:
   {
     name: 'angular-seo',
-    version: 'Angular 16.0.3',
+    version: 'Angular 16.0.4',
     bootstrap: 'Bootstrap 5.3.0',
     fontawesome: 'Font Awesome 6.4.0',
   }
