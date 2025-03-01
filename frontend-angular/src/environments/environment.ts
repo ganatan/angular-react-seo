dfd8b35bc6bc74fc5ca55d71565a745c9879e415
ganatan <dannyganatan@gmail.com>
2025-03-01 07:29:59 +0100

    Update Angular 19.2.0

--- frontend-angular/src/environments/environment.ts
+++ frontend-angular/src/environments/environment.ts
@@ -2,8 +2,8 @@ export const environment = {
     application:
     {
       name: 'angular-bootstrap',
-      version: 'Angular 19.0.1',
+      version: 'Angular 19.2.0',
       bootstrap: 'Bootstrap 5.3.3',
-      fontawesome: 'Font Awesome 6.7.1',
+      fontawesome: 'Font Awesome 6.7.2',
     }
   };
