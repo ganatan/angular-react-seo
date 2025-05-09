56aaa0dea8c2e43e8f72e05944b010623355ff07
ganatan <dannyganatan@gmail.com>
2025-05-09 06:36:25 +0200

    Update Angular 19.2.11 & Angular CLI 19.2.11

--- frontend-angular/src/environments/environment.ts
+++ frontend-angular/src/environments/environment.ts
@@ -2,8 +2,8 @@ export const environment = {
     application:
     {
       name: 'angular-bootstrap',
-      version: 'Angular 19.2.2',
-      bootstrap: 'Bootstrap 5.3.3',
+      version: 'Angular 19.2.11',
+      bootstrap: 'Bootstrap 5.3.6',
       fontawesome: 'Font Awesome 6.7.2',
     }
   };
